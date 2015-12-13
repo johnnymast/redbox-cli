b9bb9d0329562d3c9917e0a0b40c948a90dc4c96
johnny <mastjohnny@gmail.com>
2015-12-13 12:16:03 +0100

    Made code changes based on comments from scrutinizer

--- src/Arguments/Parser.php
+++ src/Arguments/Parser.php
@@ -93,7 +93,7 @@ class Parser {
 
 
         foreach ($requiredArguments as $argument) {
-            if (isset($results[$argument->prefix]) == false and isset($results[$argument->longPrefix]) == false) {
+            if (isset($results[$argument->prefix]) === false && isset($results[$argument->longPrefix]) === false) {
                 throw new \Exception(
                     'The following arguments are required: '
                     .print_r($argument->name, true).'.'
