ba989c4a673b77ec71b77cdb3a7ba1ea2db2b514
johnny <mastjohnny@gmail.com>
2015-12-13 11:44:27 +0100

    Made code changes based on comments from scrutinizer

--- src/Arguments/Argument.php
+++ src/Arguments/Argument.php
@@ -15,13 +15,13 @@ class Argument extends ArgumentObject {
     public function usageInfo() {
         $arg = array();
         if ($this->prefix) {
-            $arg[] = '-' . $this->prefix . ' ' . $this->name;
+            $arg[] = '-'.$this->prefix.' '.$this->name;
         }
         if ($this->longPrefix) {
-            $arg[] = '--' . $this->longPrefix . ' ' . $this->name;
+            $arg[] = '--'.$this->longPrefix.' '.$this->name;
         }
         if ($this->defaultValue) {
-            $arg[] = '(default: ' . $this->defaultValue . ')';
+            $arg[] = '(default: '.$this->defaultValue . ')';
         }
         if (!$this->prefix && !$this->longPrefix) {
             $arg[] = $this->name;
