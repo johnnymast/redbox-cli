2a41b6f5beeb3cffc18ea20ef135527cc3dca99f
Johnny Mast <mastjohnny@gmail.com>
2016-01-19 19:54:45 +0100

    redbox-cli-20 Cover just a litle more code

--- src/Arguments/Argument.php
+++ src/Arguments/Argument.php
@@ -30,18 +30,18 @@ class Argument extends ArgumentObject
     public function usageInfo()
     {
         $arg = array();
-        if ($this->prefix) {
+        if ($this->prefix)
             $arg[] = '-'.$this->prefix.' '.$this->name;
-        }
-        if ($this->longPrefix) {
+
+        if ($this->longPrefix)
             $arg[] = '--'.$this->longPrefix.' '.$this->name;
-        }
-        if ($this->defaultValue) {
+
+        if ($this->defaultValue)
             $arg[] = '(default: '.$this->defaultValue.')';
-        }
-        if (!$this->prefix && !$this->longPrefix) {
+
+        if (!$this->prefix && !$this->longPrefix)
             $arg[] = $this->name;
-        }
+
         $arg = implode(', ', $arg);
         return $arg;
     }
