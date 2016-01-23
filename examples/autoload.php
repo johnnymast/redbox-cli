2f228f7737dfdeff16e1330ad871582252c884b6
Johnny Mast <mastjohnny@gmail.com>
2016-01-23 12:51:37 +0100

    redbox-cli-23 Fix comments in examples/autoload.php

--- examples/autoload.php
+++ examples/autoload.php
@@ -1,8 +1,7 @@
 <?php
 /**
  * We want to make sure we load the correct autoloader
- * where ever we are. This will load the correct autoloader
- * if we are on local development or loaded as a component/package.111
+ * where ever we are.
  */
 if (is_file(__DIR__ . '/../vendor/autoload.php')) {
     require_once __DIR__ . '/../vendor/autoload.php';
