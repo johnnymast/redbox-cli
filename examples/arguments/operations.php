3af401877ccc2cdcc8453be71e05f928182458ca
Johnny Mast <mastjohnny@gmail.com>
2021-11-05 21:45:02 +0100

    Updated .travis.yml from php 7.0 to php 7.3.

--- examples/arguments/operations.php
+++ examples/arguments/operations.php
@@ -1,26 +1,12 @@
-Operations are usefully if your application supports more than one default action. Let's say you are building 
-a beautiful ftp client, and you want to give your users the option to upload or download a file from a server.
-
-This means you might want some options to be required with one action but maybe not with another action. This is the reason
-why we have added operations to the mix. 
-
-Setting and getting of arguments works the same as you would normally use them. In fact if you would set an argument without the use of operations the following code:
-
-
-```php
-$cli->arguments->get('user');
-```
-
-would alias the following code internally inside the package.
-
-```php
-$cli->arguments->getOperation("default")->get('user');
-```
-
-So let's create the scenario i mentioned above in code.
-
-```php
 <?php
+
+/**
+ * This example will show you how you can show
+ * a usage screen that informs your users on
+ * how to use your software.
+ */
+
+require __DIR__ . '/../autoload.php';
 
 use Redbox\Cli\Arguments\Option;
 use Redbox\Cli\Cli;
@@ -81,15 +67,9 @@ try {
             "The file to upload."
         );
 
-    
+
     $cli->arguments->parse();
 
 } catch (\Exception) {
     $cli->arguments->usage();
 }
-
-```
-
-
-
-
