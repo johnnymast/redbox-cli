35c66ace198508bb7175a3cc8eda54d4dce2e4b7
Johnny Mast <mastjohnny@gmail.com>
2021-12-12 14:14:41 +0100

    Updated .travis.yml from php 7.0 to php 7.3.

--- update_code_coverage.php
+++ update_code_coverage.php
@@ -12,43 +12,66 @@
  * This file updates coverage.json (used by README.md) to show the code coverage numbers
  * via img.shields.io.
  */
-$badge = [
-    "schemaVersion" => 1,
-    "label" => "Code Coverage",
-    "message" => "0%", // Will be updated by the code.
-    "color" => "blue", // Again will be updated by the code.
-    "namedLogo" => "github"
-];
-$contents = json_encode($badge);
 
-file_put_contents('coverage.json', $contents);
+try {
+
+    $badge = [
+        "schemaVersion" => 1,
+        "label" => "Code Coverage",
+        "message" => "0%", // Will be updated by the code.
+        "color" => "blue", // Again will be updated by the code.
+        "namedLogo" => "github"
+    ];
+
+    $xml = simplexml_load_string(file_get_contents('clover.xml'));
+
+    $coveredStatements = $xml->project->metrics['coveredstatements'];
+    $totalStatements = $xml->project->metrics['statements'];
+    $percentage = round(min(1, $coveredStatements / $totalStatements) * 100);
+    $percentageString = $percentage . '%';
+
+    $percentage = 5;
+
+    $badge["message"] = $percentageString;
+    $badge["color"] = match(true) {
+        $percentage > 90 => "green",
+        default => "red"
+    };
+
+    $json = json_encode($badge, JSON_THROW_ON_ERROR);
+
+    file_put_contents('coverage.json', $json);
+
+} catch (Exception $e) {
+    print_r($e->getMessage());
+}
+
+
+
+
+
+
 return;
-//exit (1);
+
+
+//$imageHeight = 20;
+//$imageWidth = 170;
+//$xMargin = 5;
+//$font = 3;
 //
-//$xml = simplexml_load_file('clover.xml');
+//$image = imagecreate($imageWidth, $imageHeight);
 //
-$coveredStatements = $xml->project->metrics['coveredstatements'];
-$totalStatements = $xml->project->metrics['statements'];
-$percentage = round(min(1, $coveredStatements / $totalStatements) * 100);
-$percentageString = $percentage . '%';
-$imageHeight = 20;
-$imageWidth = 170;
-$xMargin = 5;
-$font = 3;
-
-$image = imagecreate($imageWidth, $imageHeight);
-
-$backgroundColor = imagecolorallocate($image, 0, 20, 0);
-$foregroundColor = imagecolorallocate($image, 0, 245, 0);
-$borderColor = imagecolorallocate($image, 0, 190, 0);
-
-imagerectangle($image, 0, 0, $imageWidth - 1, $imageHeight - 1, $borderColor);
-
-imagestring($image, $font, $xMargin, 3, 'Pest Code Coverage:', $foregroundColor);
-
-$width = imagefontwidth($font) * strlen($percentageString);
-imagestring($image, $font,$imageWidth - $xMargin - $width, 3, $percentageString, $foregroundColor);
-
-imagepng($image, 'docs/code-coverage.png');
-
-imagedestroy($image);
+//$backgroundColor = imagecolorallocate($image, 0, 20, 0);
+//$foregroundColor = imagecolorallocate($image, 0, 245, 0);
+//$borderColor = imagecolorallocate($image, 0, 190, 0);
+//
+//imagerectangle($image, 0, 0, $imageWidth - 1, $imageHeight - 1, $borderColor);
+//
+//imagestring($image, $font, $xMargin, 3, 'Pest Code Coverage:', $foregroundColor);
+//
+//$width = imagefontwidth($font) * strlen($percentageString);
+//imagestring($image, $font,$imageWidth - $xMargin - $width, 3, $percentageString, $foregroundColor);
+//
+//imagepng($image, 'docs/code-coverage.png');
+//
+//imagedestroy($image);
